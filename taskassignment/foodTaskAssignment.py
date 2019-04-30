42d186845df519c3b5bd1bc104dd2e7a9c1063c0
Enreina Annisa Rizkiasri <enreina@gmail.com>
2019-04-30 18:07:21 +0200

    food task assignment

--- taskassignment/foodTaskAssignment.py
+++ taskassignment/foodTaskAssignment.py
@@ -1,41 +1,168 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from db.firestoreClient import db
+from firebase_admin import firestore
+from common.constants import taskType
+from datetime import datetime, timedelta
+from dateutil.tz import tzlocal 
 
 foodTaskAssignment = Blueprint('foodTaskAssignment', __name__)
 
 # FOOD TASK GENERATION & ASSIGNMENT
 # hit the endpoint by mobile app and chatbot after creation
-@foodTaskAssignment.route('/api/food/generate-enrichment-task/<itemId>')
+@foodTaskAssignment.route('/api/food/generate-enrichment-task/<itemId>', methods=['GET', 'POST'])
 def generateFoodEnrichmentTask(itemId):
-    # 
-    # call assign foodtask after task is generated
-    return {'methodName': 'generateFoodEnrichmentTask', 'itemId': itemId} #replace this
+    if request is not None and request.method == 'GET':
+        return {'methodName': 'generateFoodEnrichmentTask'}
+    else:
+        # get the item
+        item = db.collection('foodItems').document(itemId)
+        itemDict = item.get().to_dict()
+        # generate the enrichment task
+        answersCount = {
+            'mealCategory': []
+        }
+    
+        taskData = {
+            'itemId': itemId,
+            'item': item,
+            'type': taskType.TASK_TYPE_ENRICH_ITEM,
+            'numOfAnswersRequired': 5,
+            'createdAt': datetime.now(tzlocal()),
+            'expirationDate': None, # no expiration date
+            'answersCount': answersCount
+        }
+        taskId = db.collection('foodTasks').add(taskData)
+        # call assign foodtask after task is generated
+        assignFoodTask(taskId[1].id)
 
-@foodTaskAssignment.route('/api/food/assign-enrichment-task/<itemId>')
-def assignFoodEnrichmentTask(itemId):
-    # load all users except the author
-    # order by totalTasksCompleted (ascending)
-    # limit num of users to numOfRequiredAnswers * 2
-
-    # generate task instance to each user
-    return {'methodName': 'assignFoodEnrichmentTask', 'itemId': itemId} #replace this
+        del taskData['item']
+        return {'taskId': taskId[1].id, 'task': taskData}
 
 # hit the endpoint by mobile app and chatbot after enrichment task is completed
-@foodTaskAssignment.route('/api/food/generate-validation-task/<enrichmentTaskId>')
-def generateFoodValidationTask(enrichmentTaskId):
-    # check if aggregatedAnswers >= numOfRequiredAnswer
-    # AND all task instances are completed
+@foodTaskAssignment.route('/api/food/generate-validation-task/<userId>/<enrichmentTaskInstanceId>', methods=['GET', 'POST'])
+def generateFoodValidationTask(userId, enrichmentTaskInstanceId):
+    if request is not None and request.method == 'GET':
+        return {'methodName': 'generateFoodValidationTask'}
 
-    # count the majority of answer (include answers from created item)
-    # generate task.data according to majority count
+    # get enrichment task instance
+    taskInstanceRef = db.collection('users').document(userId).collection('foodTaskInstances').document(enrichmentTaskInstanceId)
+    taskInstance = taskInstanceRef.get().to_dict()
+    # get enrichment answers
+    foodEnrichments = db.collection('foodEnrichments').where(u'taskInstance', u'==', taskInstanceRef).get()
+    foodEnrichment = [x for x in foodEnrichments][0].to_dict()
+    # get task
+    task = taskInstance['task'].get()
+    taskId = task.id
+    taskDict = task.to_dict()
+    # get task item
+    itemRef = taskDict['item']
+    item = itemRef.get()
+    itemDict = item.to_dict()
+    # update answers count
+    answersCount = taskDict['answersCount']
+    answersCountSufficient = True
+    majorityAnswers = {}
 
-    # call assign food validation task
-    return {'methodName': 'generateFoodValidationTask', 'enrichmentTaskId': enrichmentTaskId} #replace this
+    newCount = []
+    for mealCategory in foodEnrichment['mealCategory']:
+        newMealCategoryCount = []
+        added = False
+        counter = 0
+        maxCount = 0
+        for mealCategoryCountObject in answersCount['mealCategory']:
+            isPropertyNameEqual = mealCategoryCountObject['propertyName'] == mealCategory['propertyName']
+            isPropertyValueEqual = mealCategoryCountObject['propertyValue'] == mealCategory['propertyValue']
+            
+            if isPropertyNameEqual and isPropertyValueEqual:
+                count = mealCategoryCountObject['propertyCount'] + 1
+                added = True
+                mealCategoryCountObject['propertyCount'] = count
+                counter = counter + count
+                if count > maxCount:
+                    maxCount = count
+                    majorityAnswers[mealCategory['propertyName']] = mealCategory['propertyValue']
+            elif isPropertyNameEqual:
+                counter = counter + mealCategoryCountObject['propertyCount']  
+        
+        if not added:
+            answersCount['mealCategory'].append(
+                {'propertyName': mealCategory['propertyName'], 
+                'propertyValue': mealCategory['propertyValue'], 
+                'propertyCount': 1})
+            counter = counter + 1
+            if maxCount == 0:
+                majorityAnswers[mealCategory['propertyName']] = mealCategory['propertyValue']
+        answersCountSufficient = answersCountSufficient and (counter >= taskDict['numOfAnswersRequired'])
+        
+    # update answers count in DB
+    taskInstance['task'].update({'answersCount': answersCount})    
+    
+    # check if each answer count >= numOfRequiredAnswer
+    if answersCountSufficient:
+        # create aggregated answers dict
+        aggregatedAnswers = {
+            'imageUrl': itemDict['imageUrl'],
+            'name': itemDict['name'],
+            'mealItems': itemDict['mealItems']
+        }
 
-@foodTaskAssignment.route('/api/food/assign-validation-task/<taskId>')
-def assignFoodValidationTask(taskId):
-    # load all users except the author
-    # order by totalTasksCompleted (ascending)
-    # limit num of users to numOfRequiredAnswers
+        # generate task.aggregatedAnswers according to majority count
+        aggregatedAnswers['mealCategory'] = []
+        for mealItem in majorityAnswers:
+            aggregatedAnswers['mealCategory'].append({
+                'propertyName': mealItem,
+                'propertyValue': majorityAnswers[mealItem]
+            })
+        
+        # generate task
+        taskData = {
+            'itemId': item.id,
+            'item': itemRef,
+            'type': taskType.TASK_TYPE_VALIDATE_ITEM,
+            'numOfAnswersRequired': 5,
+            'createdAt': datetime.now(tzlocal()),
+            'expirationDate': None, # decide expiration date
+            'aggregatedAnswers': aggregatedAnswers
+        }
+        taskId = db.collection('foodTasks').add(taskData)
+        # call assign foodTask after task is generated
+        assignFoodTask(taskId[1].id)
 
-    # generate task instance to each user
-    return {'methodName': 'assignFoodValidationTask', 'taskId': taskId} #replace this
+        del taskData['item']
+        del taskData['aggregatedAnswers']
+        return {'taskId': taskId[1].id, 'task': taskData}
+
+    return {'message': 'Validation task was not generated'}
+
+@foodTaskAssignment.route('/api/food/assign-task/<taskId>', methods=['GET', 'POST'])
+def assignFoodTask(taskId):
+    if request is not None and request.method == 'GET':
+        return {'methodName': 'assignFoodTask'}
+
+    # get task and item
+    taskRef = db.collection('foodTasks').document(taskId)
+    task = taskRef.get().to_dict()
+    item = task['item'].get().to_dict()
+    # get item's author
+    authorId = item['authorId']
+    taskInstance = {
+        'taskId': taskId,
+        'task': taskRef,
+        'createdAt': datetime.now(tzlocal()),
+        'completed': False,
+        'expired': False,
+        'expirationDate': task['expirationDate']
+    }
+    
+    users = db.collection('users').order_by('totalTasksCompleted.food', direction=firestore.Query.ASCENDING).get()
+    users = [x.id for x in users]
+    numOfUsersNeeded = task['numOfAnswersRequired'] * 3
+
+    counter = 0
+    for userId in users[:numOfUsersNeeded]:
+        if userId != authorId:
+            taskInstanceCollection = db.collection('users').document(userId).collection('foodTaskInstances')
+            taskInstanceCollection.add(taskInstance)
+            counter = counter + 1
+
+    return {'message': "Task instance generated for {counter} users".format(counter=counter)}
